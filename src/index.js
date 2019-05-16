0309e7d63e0b1b8693de1934fa6e6a8bd44fc236
Dave Mizrahi <davem@Daves-MBP.home>
2019-05-16 15:39:56 -0400

    Modified port handling

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 /* eslint-disable no-console */
 const logger = require('./logger');
 const app = require('./app');
-const port = app.get('port');
+// const port = app.get('port');
+// Added to make it work on Heroku
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+// app.listen(port);
+
 const server = app.listen(port);
 
 process.on('unhandledRejection', (reason, p) =>
